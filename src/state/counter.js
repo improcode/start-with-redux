aa00789b2a577d3a0970212f949a9e47891005f5
improcode <art.milkowski@gmail.com>
2018-05-14 13:00:50 +0200

    todo list

--- src/state/counter.js
+++ src/state/counter.js
@@ -2,19 +2,19 @@ const INC = 'counter/INC'
 const DEC = 'counter/DEC'
 
 //action creators declarations
-export const inc = () => ({type: INC})
-export const dec = () => ({type: DEC})
+export const inc = (number = 1) => ({type: INC, number: number})
+export const dec = (number = 1) => ({type: DEC, number: number})
 
-//initial state - stte that is present before any action
+//initial state - state that is present before any action
 const initialState = 0
 
 // state = initialState -> jeśli nie ma nic w stanie to użyj wartości initialState
 export default (state = initialState, action) => {
     switch (action.type) {
         case INC:
-            return state + 1
+            return state + action.number
         case DEC:
-            return state - 1
+            return state - action.number
         default:
             return state
     }
--- src/store.js
+++ src/store.js
@@ -1,17 +1,22 @@
 import {createStore, combineReducers} from 'redux'
-import counterReducer, {inc, dec} from './state/counter'
+import counter, {inc, dec} from './state/counter'
+import todos, {add, del} from './state/todos'
 
 
 const reducer = combineReducers({
- counter: counterReducer
+    counter,
+    todos
+
 })
 
 export const store = createStore(
     reducer,
+
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-window.inc = () => store.dispatch(inc())
-window.inc = () => store.dispatch(inc())
-window.inc = () => store.dispatch(inc())
-window.dec = () => store.dispatch(dec())
+window.inc = (number) => store.dispatch(inc(number))
+window.dec = (number) => store.dispatch(dec(number))
+
+window.addTask = (taskText) => store.dispatch(add(taskText))
+// window.addTask = (taskText) => store.dispatch(add(taskText))
